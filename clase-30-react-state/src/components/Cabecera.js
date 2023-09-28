a8e9e830aff5bb7e24f62b7af53c91df66a47b63
L1cha <lisandro14_2000@hotmail.com>
2023-09-27 22:05:04 -0300

    react

---
+++ clase-30-react-state/src/components/Cabecera.js
@@ -0,0 +1,27 @@
+import "bootstrap/dist/css/bootstrap.css";
+import PropTypes from 'prop-types';
+
+function Cabecera({titulo, subtitulo, imagen}) {  //Lo genere poniendo ffc y enger
+    return (<header className="alert bg-dark text-danger text-center">
+        <img src={imagen} alt="logo" className="float-start" style={{width:"10vw"}}/>
+        <img src={imagen} alt="logo" className="float-end" style={{width:"10vw"}}/>
+        <h1>{titulo}</h1>
+        <h2>{subtitulo}</h2>
+    </header>);
+}
+
+Cabecera.propTypes = {
+    titulo: PropTypes.string.isRequired,
+    subtitulo: PropTypes.string.isRequired,
+    imagen: PropTypes.string // Propiedad 'imagen' opcional
+}
+
+
+export default Cabecera;
+
+
+//Snppets
+//ffc  : Componente Funcional
+//ccc  : Componente de Clases con Constructor
+//cc   : Componente de Clases
+//sfc  : Componente Funcional Flecha
